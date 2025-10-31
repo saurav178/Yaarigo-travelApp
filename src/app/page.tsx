678bb0337a89505ce395cf24ecb95b91f93dba77
saurav <sauravojha178@gmail.com>
2025-10-31 18:17:38 +0530

    add new landing-page and make changes

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import LandingPage from "./landing-page/LandingPage";
+import Header from "../components/Header";
+
+export default function Page() {
+  return (
+    <>
+      <Header />
+      <LandingPage />
+    </>
+  );
+}
