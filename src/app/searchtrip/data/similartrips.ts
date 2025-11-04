0e6fa62b21778d6d2845f7070f4e655366f0b9b5
Asajid4u <asajid4u@gmail.com>
2025-11-04 08:09:59 +0530

    nothing

--- src/app/searchtrip/data/similartrips.ts
+++ src/app/searchtrip/data/similartrips.ts
@@ -8,5 +8,5 @@ export const SAMPLE_DATA_02: Similar[] = [
   { id: "4", name: "Amit singh",age:40, rating: 4.4, location: "Mumbai, India", from: "Dubai, UAE", to: "Abu Dhabi, UAE" , tags: ["Trek","Adventure"], match: 82, price: "₹9k - ₹25k", days: 7, avatar: "/avatar2.jpg", cover: "/searchpageimg/cover1.jpg" },
   { id: "5", name: "Rohit Sharma", age:50, rating: 4.5, location: "Mumbai, India", from: "Rome, Italy", to: "Venice, Italy", tags: ["History"], match: 78, price: "₹12k -₹30k", days: 6, avatar: "/avatar4.jpg", cover: "/searchpageimg/cover1.jpg" },
   { id: "6", name: "Amit singh",age:60, rating: 4.6, location: "Goa, India",  from: "Berlin, Germany", to: "Munich, Germany", tags: ["Beach","Photography"], match: 88, price: "₹15k - ₹50k", days: 4, avatar: "/avatar1.jpg", cover: "/searchpageimg/cover1.jpg" },
-  { id: "7", name: "Nisha Kapoor", age:45, rating: 4.7, location: "Goa, India", from: "New York, USA", to: "Los Angeles, USA", tags: ["Foodie","Culture"], match: 88, price: "₹11k", days: 5, avatar: "/avatar5.jpg", cover: "/searchpageimg/cover1.jpg" },
+  { id: "7", name: "Nisha Kapoor", age:45, rating: 4.7, location: "Goa, India", from: "Simla, Himachal Pradesh, India", to: "Mumbai, India" ,tags: ["Foodie","Culture"], match: 88, price: "₹11k", days: 5, avatar: "/avatar5.jpg", cover: "/searchpageimg/cover1.jpg" },
 ];
