3cc71e9d2e03bc8ddc25fdfaa4a8bc8fd264c098
Abhay Kholia <kholiaabhay@gmail.com>
2025-11-06 12:08:55 +0530

    TripView

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,13 @@
 "use client";
 
 import LandingPage from "./landing-page/LandingPage";
-import TripDetailsPage from "./TripDetails/TripDetailsPage";
-import HeroSection from "./HeroSection";
-import TripDetails from "./triphighlight/TripDetails";
+
 
 export default function Page() {
   return (
     <div className="relative min-h-screen overflow-visible">
       <LandingPage />
-       <HeroSection />
-      <TripDetailsPage/>
-      <TripDetails/>
+     
     </div>
   );
 }
