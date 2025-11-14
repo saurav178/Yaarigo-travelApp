97de1ab78b624950e453511a0593bff5633e6a9e
Abhay Kholia <kholiaabhay@gmail.com>
2025-11-14 15:22:05 +0530

    letest

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import GlobalLayout from "../components/layout/GlobalLayout";
+import GlobalLayout from "./components/layout/GlobalLayout";
 import { AuthProvider } from "../context/AuthContext";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
