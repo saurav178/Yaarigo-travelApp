ae14105c65ec47aeb5f250e3f88d1f6859da6988
megha03-coder <meghachauhan310@gmail.com>
2025-11-14 13:54:44 +0530

    modify profile section

--- src/app/profile/components/AboutSection.tsx
+++ src/app/profile/components/AboutSection.tsx
@@ -7,12 +7,12 @@ export default function AboutSection() {
       className="w-full md:w-96 md:self-start bg-white shadow-lg rounded-2xl p-4"
       style={{ boxShadow: "4px 0 8px rgba(0,0,0,0.1)" }}
     >
-      <h3 className="text-lg font-semibold mb-1">About</h3>
+      <h3 className="text-lg  mb-1">About</h3>
       <p className="text-gray-600 text-sm">
         Wildlife photographer and nature lover. Eco-conscious traveler.
       </p>
 
-      <h4 className="font-semibold mt-2 mb-4 flex items-center gap-1">
+      <h4 className=" mt-2 mb-4 flex items-center gap-1">
         <Image
           src="/language.png"
           alt="Languages"
@@ -33,7 +33,7 @@ export default function AboutSection() {
         ))}
       </div>
 
-      <h4 className="font-semibold mt-2 mb-1"> Travel Style</h4>
+      <h4 className=" mt-2 mb-1"> Travel Style</h4>
       <div className="flex flex-wrap gap-2">
         {["Adventure", "Cultural", "Budget-friendly"].map((style) => (
           <span
@@ -45,7 +45,7 @@ export default function AboutSection() {
         ))}
       </div>
 
-      <h4 className="font-semibold mt-2 mb-1"> Interests</h4>
+      <h4 className=" mt-2 mb-1"> Interests</h4>
       <div className="flex flex-wrap gap-2">
         {["Hiking", "Photography", "Local cuisine"].map((interest) => (
           <span
