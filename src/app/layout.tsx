3b1cbc06b87b3bdf8da335ee7f821e37fea10017
Smrutisaurav Ojha <sauravojha178@gmail.com>
2026-02-14 20:18:41 +0530

    fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,7 +39,6 @@ export default function RootLayout({
             <Toaster position="top-right" />
 
             <Footer />
-            <Footer/>
           </ModalProvider>
         </AuthProvider>
       </body>
