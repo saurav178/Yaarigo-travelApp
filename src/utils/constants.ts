4b0976710d2f092d7fa56ee5754d54e3397fb626
Megha03-coder <meghachauhan310@gmail.com>
2026-02-19 18:34:07 +0530

    minor updates

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,7 +3,7 @@ import { config } from "@/utils/env";
 export const API_BASE_URL = config.API_BASE_URL;
 
 export const API_ENDPOINTS = {
-  PACKAGES: `${API_BASE_URL}/tpm-service/api/public/packages`,
+  PACKAGES: `${API_BASE_URL}/packages`,
   LOCATION_SEARCH: "/api/location",
 };
 
