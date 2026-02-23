a3f83160e46479e1d65b20e2a6e2cd8870f5d6ca
Megha03-coder <meghachauhan310@gmail.com>
2026-02-23 20:55:53 +0530

    minor updates

--- src/utils/apiConfig.ts
+++ src/utils/apiConfig.ts
@@ -1,10 +1,10 @@
-export const API_BASE_URL = "https://api.dev.yaarigo.com";
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 export const API_ENDPOINTS_CONFIG = {
   BOOKING: {
     CREATE_CART: `${API_BASE_URL}/booking-service/booking-cart/active`,
     TRAVELER_PROFILES: `${API_BASE_URL}/booking-service/traveler-profiles`,
     ADD_TRAVELERS: (cartId: string) => `${API_BASE_URL}/booking-service/booking-cart/${cartId}/travelers`,
-    REMOVE_TRAVELERS: (cartId: string, travelerId: string) => `${API_BASE_URL}/booking-service/booking-cart/${travelerId}/travelers`,
+    REMOVE_TRAVELERS: (cartId: string, travelerId: string) => `${API_BASE_URL}/booking-service/booking-cart/${cartId}/travelers/${travelerId}`,
   }
 };
